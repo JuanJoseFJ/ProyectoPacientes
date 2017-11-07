0e1cbd7b0dd9d66a7f574d000e55222b9b31f041
JuanJoseFJ <30563275+JuanJoseFJ@users.noreply.github.com>
2017-11-06 21:13:21 -0600

    eliminacion de proyecto anterio

--- ControlPacientes/src/Modelo/IrtDoctor.java
+++
@@ -1,9 +0,0 @@
-package Modelo;
-
-public interface IrtDoctor {
-    public void AgregarMedico(Medico nuevoMedico);
-    public void BuscarMedico(String Apellidos);
-    public void EditarMedico(String Apellidos);
-    public void EliminarMedico(String Apellidos);
-        
-}
--- ControlPacientes/src/Modelo/IrtPaciente.java
+++
@@ -1,9 +0,0 @@
-package Modelo;
-
-
-public interface IrtPaciente {
-    public void AgregarPaciente(Paciente nuevoPaciente);
-    public void BuscarPaciente(String Apellidos);
-    public void EditarPaciente(String Apellidos);
-    public void EliminarPaciente(String Apellidos);
-}
